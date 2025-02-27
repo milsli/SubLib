18d0397aaee65c54ec65033ead2071d4f4df396d
Miłosz <milsli69.gmail.com>
2025-02-27 15:56:09 +0100

    Dodanie nowej funkcji do biblioteki

--- sublib.cpp
+++ sublib.cpp
@@ -1,3 +1,8 @@
 #include "sublib.h"
 
 SubLib::SubLib() {}
+
+std::string SubLib::retFunction1()
+{
+    return "To pierwsza funkcja z biblioteki SubLib";
+}
--- sublib.h
+++ sublib.h
@@ -2,11 +2,15 @@
 #define SUBLIB_H
 
 #include "SubLib_global.h"
+#include <string>
 
 class SUBLIB_EXPORT SubLib
 {
 public:
     SubLib();
+
+    std::string retFunction1();
+
 };
 
 #endif // SUBLIB_H
